abf4eeeabe37c8e8b7fe71bf815adfc5a9f670d6
joemarvi <joelson.dh@gmail.com>
2024-05-12 04:47:49 +0800

    Update packages Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './pages/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DebtsComponent } from './pages/debts/debts.component';
+import { ExpensesComponent } from './pages/expenses/expenses.component';
+import { PlansComponent } from './pages/plans/plans.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'expenses', component: ExpensesComponent },
+    { path: 'debts', component: DebtsComponent },
+    { path: 'plans', component: PlansComponent },
+];
